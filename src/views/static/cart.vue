75e801bb74e131e33478e18dc288e37656e6770a
贺园媛 <507961642@qq.com>
2023-05-19 18:32:43 +0800

    2003010103 贺园媛

--- src/views/static/cart.vue
+++ src/views/static/cart.vue
@@ -1,5 +1,5 @@
 <template>
-    <Cart></Cart>
+  <Cart>购物车</Cart>
 </template>
 
 <script>
@@ -8,50 +8,49 @@ import { defineComponent } from "vue"
 import Cart from "../../components/Cart.vue";
 export default defineComponent({
 
-    data() {
-        return{
+  data () {
+    return {
 
-        }
-    },
-    methods: {
-    },
-    components:{
-        Cart
     }
+  },
+  methods: {
+  },
+  components: {
+    Cart
+  }
 
 });
 </script>
 
 <style lang="scss" scoped>
 .product {
-    display: flex; //弹性布局
-    flex-direction: column;
-    margin: 20px 10px;
+  display: flex; //弹性布局
+  flex-direction: column;
+  margin: 20px 10px;
 
-    .name {
-        font-size: 16px;
-        font-weight: bold;
+  .name {
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  .bars {
+    display: flex; //嵌套
+    justify-content: space-between; //两边对齐
+    height: 35px;
+    align-items: center; //垂直居中对齐
+    margin-top: 10px;
+
+    .price {
+      color: red;
     }
-
-    .bars {
-        display: flex; //嵌套
-        justify-content: space-between; //两边对齐
-        height: 35px;
-        align-items: center; //垂直居中对齐
-        margin-top: 10px;
-
-
-        .price {
-            color: red;
-        }
-    }
+  }
 }
 
 .total {
-    display: flex;
-    margin: 20px 10px 0 10px;
-    justify-content: space-between;
-    align-items: center;
-    height: 40px;
+  display: flex;
+  margin: 20px 10px 0 10px;
+  justify-content: space-between;
+  align-items: center;
+  height: 40px;
 }
 </style>
