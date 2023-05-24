9c7f1bb3a841da1f2f961aa55a4208c7192ba805
ync22 <12882227+ync22@user.noreply.gitee.com>
2023-05-24 21:23:12 +0800

    2003010121游南城

--- src/http/major.js
+++ src/http/major.js
@@ -1,58 +1,117 @@
-//统一api管理
-import instance from "./axios";
-// data json
-const userLogin=(data)=>{
-    //返回Promise对象
-   return instance.request({
-        url:'api/mobile/elogin', //fullpath=baseURL+url  http://api.jqrjq.cn/api/mobile/elogin
-        method:'post',
-        data,
-      
-    })
-}
-const adminLogin=(data)=>{
-   return instance.request({
-    url:'', //url=baseURL+url
-        method:'post',
-        data
-   })
-}
-const adCatePage=(data)=>{
-   return instance.request({
-    url:'api/adcate/page',
-    method:'post',
-    data
-   })
-}
-const adCateDelId=(params)=>{
-    return instance.request({
-        url:'api/adcate/delete',
-        params
-    })
-}
-const adCateAdd=(data)=>{
-    return instance.request({
-        url:'api/adcate/add',
-        method:'post',
-        data
-    })
-}
-const adCateEdit=(data)=>{
-    return instance.request({
-        url:'api/adcate/edit',
-        method:'post',
-        data
-    })
+import { ElLoading } from 'element-plus'
+import api from './axios.js'
+import { useUserStore } from '../store/user.js'
+const userStore = useUserStore() // 获取用户 store
+const token = userStore.token // 获取 token
+const headers = {
+  Authorization: 'Bearer ' + token, // 添加 Authorization 头部字段
 }
 
-// const isEven = n => n % 2 === 0;
+let loadingInstance // 定义加载实例
+
+const showLoading = () => {
+  // 创建加载实例并显示加载动画
+  loadingInstance = ElLoading.service({
+    lock: true,
+    text: '加载中...', // 可自定义加载提示文本
+  })
+}
+
+const hideLoading = () => {
+  // 关闭加载实例，隐藏加载动画
+  if (loadingInstance) {
+    loadingInstance.close()
+  }
+}
+
+const requestWithLoading = async (config) => {
+  showLoading() // 显示加载动画
+
+  try {
+    return await api.request(config) // 发起接口请求
+  } finally {
+    hideLoading() // 隐藏加载动画
+  }
+}
+
+const school = async (data) => {
+  return await requestWithLoading({
+    url: '/api/school/all',
+    method: 'get',
+    headers,
+    data,
+  })
+}
+
+const department = async (data) => {
+  return await requestWithLoading({
+    url: '/api/department/all',
+    method: 'get',
+    headers,
+    data,
+  })
+}
+
+const getMajor = async (data) => {
+  return await requestWithLoading({
+    url: '/api/major/all',
+    method: 'get',
+    headers,
+    data,
+  })
+}
 
 
-export {
-    userLogin,
-    adminLogin,
-    adCatePage,
-    adCateDelId,
-    adCateAdd,
-    adCateEdit
-} 
+const majorAdd = async (data) => {
+  return await requestWithLoading({
+    url: '/api/major/add',
+    method: 'post',
+    headers,
+    data,
+  })
+}
+
+const majorEdit = async (data) => {
+  return await requestWithLoading({
+    url: '/api/major/edit',
+    method: 'post',
+    headers,
+    data,
+  })
+}
+
+
+const getMajorsForSelectBySchoolId = async (data) => {
+  return await requestWithLoading({
+    url: `/api/major/getMajorsForSelectBySchoolId?schoolId=${data.schoolId}`,
+    method: 'get',
+    headers,
+  })
+}
+
+
+
+const majorDelete = async (data) => {
+  return await requestWithLoading({
+    url: `/api/major/delete?id=${data.id}`,
+    method: 'get',
+    headers,
+
+  })
+}
+
+
+
+
+
+
+
+export default {
+  school,
+  department,
+  majorAdd,
+  getMajor,
+  majorEdit,
+  majorDelete,
+  getMajorsForSelectBySchoolId
+}
